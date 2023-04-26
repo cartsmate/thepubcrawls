a70ef17238104f0d82b15291fdad1d50c78d1fd8
Andy Carter <andyjcarter77@gmail.com>
2023-04-26 17:18:32 +0100

    fix category lists

--- app/static/javascript/html/filter_table.js
+++ app/static/javascript/html/filter_table.js
@@ -41,12 +41,18 @@ function Category() {
     document.getElementById("table-pubs-json").innerHTML = create_table(pubs_reviews)
     $(document).ready(function () {
         $('#pub_list').DataTable({
-            order: [[5, 'desc']],
+            paging: false,
+            info: false,
+            order: [[7, 'desc']],
             columnDefs: [
+                    {target: 0, visible: true, searchable: true, },
+                    {target: 1, visible: true, searchable: false, },
                     {target: 2, visible: false, searchable: false, },
                     {target: 3, visible: false, searchable: false, },
                     {target: 4, visible: false, searchable: false, },
                     {target: 5, visible: false, searchable: false, },
+                    {target: 6, visible: true, searchable: true, },
+                    {target: 7, visible: true, searchable: true, }
                         ]
         });
     });
