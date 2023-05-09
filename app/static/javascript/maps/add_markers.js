9b62f576bfc21df3225691afa16737eb7b887771
Andy Carter <andyjcarter77@gmail.com>
2023-05-09 22:18:53 +0100

    no marker clicks on home page

--- app/static/javascript/maps/add_markers.js
+++ app/static/javascript/maps/add_markers.js
@@ -1,4 +1,4 @@
-function add_markers(map, zoom, data) {
+function add_markers(map, zoom, data, clickable) {
     console.log('add_markers')
     var pinSVGHole = "M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z";
     var labelOriginHole = new google.maps.Point(12,15);
@@ -8,7 +8,8 @@ function add_markers(map, zoom, data) {
     var marker, i, j;
     for (var key in data) {
         //console.log(data[key])
-        var pinColor = data[key].colour
+        //var pinColor = data[key].colour
+        var pinColor = 'red'
         if (zoom < 15) {
             var pinHole = pinSVGFilled
             var label = {
@@ -47,33 +48,35 @@ function add_markers(map, zoom, data) {
             icon: markerImage,
             title: title_name
         })
-        if (zoom < 15) {
-            google.maps.event.addListener(marker, 'click', (function (marker, key) {
-                return function () {
-                    set=data[key].area.trim().replace(/%20/g, " ");
-                    console.log(data[key].area)
-                    console.log(set)
-                    infowindow.x = set;
-                    infowindow.setContent("<p><b>" + set + "</b></p>" +
-                        "<a href='/pub/list/area/" + set + "'>list of local venues</a>");
-                    infowindow.open(map, marker);
-                }
-            })(marker, key));
-        } else {
-            google.maps.event.addListener(marker, 'click', (function (marker, key) {
-                return function () {
-                    infowindow.x = data[key].name;
-                    infowindow.setContent("<p><b>" + data[key].name + "</b></p>" +
-                        "<p>address : " + data[key].address + "</p>" +
-                        "<p>category : " + data[key].category + "</p>" +
-                        "<p>score : " + data[key].score + "</p>" +
-                        "<p>best for : " + data[key].star + "</p>" +
-                        "<p>station : " + data[key].station + "</p>" +
-                        "<p>area : " + data[key].area + "</p>" +
-                        "<a href='/pub/" + data[key].pub_identity + "'>click for more details</a>");
-                    infowindow.open(map, marker);
-                }
-            })(marker, key));
+        if (clickable == true) {
+            if (zoom < 15) {
+                google.maps.event.addListener(marker, 'click', (function (marker, key) {
+                    return function () {
+                        set=data[key].area.trim().replace(/%20/g, " ");
+                        console.log(data[key].area)
+                        console.log(set)
+                        infowindow.x = set;
+                        infowindow.setContent("<p><b>" + set + "</b></p>" +
+                            "<a href='/pub/list/area/" + set + "'>list of local venues</a>");
+                        infowindow.open(map, marker);
+                    }
+                })(marker, key));
+            } else {
+                google.maps.event.addListener(marker, 'click', (function (marker, key) {
+                    return function () {
+                        infowindow.x = data[key].name;
+                        infowindow.setContent("<p><b>" + data[key].name + "</b></p>" +
+                            "<p>address : " + data[key].address + "</p>" +
+                            "<p>category : " + data[key].category + "</p>" +
+                            "<p>rank : " + data[key].rank + "</p>" +
+                            "<p>best for : " + data[key].star + "</p>" +
+                            "<p>station : " + data[key].station + "</p>" +
+                            "<p>area : " + data[key].area + "</p>" +
+                            "<a href='/pub/" + data[key].pub_identity + "'>click for more details</a>");
+                        infowindow.open(map, marker);
+                    }
+                })(marker, key));
+            }
         }
     }
     return marker
