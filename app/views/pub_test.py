be4ba67b75b098eb893848bfcdb5daf52b2f019c
Andy Carter <andyjcarter77@gmail.com>
2023-05-30 14:59:26 +0100

    bug fix edit

--- app/views/pub_test.py
+++ app/views/pub_test.py
@@ -8,9 +8,10 @@ from config import Configurations
 config = Configurations().get_config()
 
 
-@app.route("/")
 @app.route("/test")
 def pub_test(x, y):
     print('/test')
     return x + y
     # return render_template('a_test.html', google_key=config['google_key'])
+
+
