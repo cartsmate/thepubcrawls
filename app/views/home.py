a589fdc012f450fb6a9d49a7d67bf5ea67714ee5
Andy Carter <andyjcarter77@gmail.com>
2023-06-14 18:18:17 +0100

    remove unused modules

--- app/views/home.py
+++ app/views/home.py
@@ -1,14 +1,8 @@
-import os
-import json
 import random
 
 import botocore.exceptions
 import pandas as pd
-# import geocoder
-from configparser import ConfigParser
-
-import requests
-from flask import render_template, redirect, url_for, g, session
+from flask import render_template
 from app import app
 from config import Configurations
 from app.static.pythonscripts.dataframes import Dataframes
@@ -21,15 +15,6 @@ config = Configurations().get_config()
 config2 = Configurations().get_config2()
 
 
-# @app.route("/home/<on_line>")
-# def home(on_line):
-    # if session.get('logged_in') != True:
-    #     return redirect(url_for('login'))
-    # if on_line == 'true':
-    #     Functions().get_s3_pubs().to_csv(os.getcwd() + '/files/' + config['pub']['aws_key'], sep=',', encoding='utf-8', index=False)
-    #     Functions().get_s3_reviews().to_csv(os.getcwd() + '/files/' + config['review']['aws_key'], sep=',', encoding='utf-8', index=False)
-    #     Functions().get_s3_areas().to_csv(os.getcwd() + '/files/' + config['area']['aws_key'], sep=',', encoding='utf-8', index=False)
-    #     Functions().get_s3_stations().to_csv(os.getcwd() + '/files/' + config['station']['aws_key'], sep=',', encoding='utf-8', index=False)
 @app.route("/home/")
 def home():
     if config2['env'] == 'prod':
