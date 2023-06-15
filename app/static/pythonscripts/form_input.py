3a6624bc6c03bc29048636786564f882406a0148
Andy Carter <andyjcarter77@gmail.com>
2023-06-15 18:48:01 +0100

    update to detail

--- app/static/pythonscripts/form_input.py
+++ app/static/pythonscripts/form_input.py
@@ -17,27 +17,30 @@ class FormInput:
         return df_pubs
 
     def get_review(self, df_reviews, pub_id):
+        ignore_list = ['review_deletion', 'review_identity', 'pub_identity', 'detail']
         print('FormInput: get_review')
-        # for review in list(Review2().__dict__.keys()):
-        #     if review not in self.ignore_list:
-        #         print(review)
-        #         df_reviews.loc[df_reviews['pub_identity'] == pub_id, review] = True if request.form.get(review) == 'on' else False
-        print('roast')
-        print(df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'roast'])
-        print('value')
-        print(request.form.get('roast'))
-
-        df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'brunch'] = True if request.form.get('brunch') == 'on' else False
-        df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'dart'] = True if request.form.get('dart') == 'on' else False
-        df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'entertain'] = True if request.form.get('entertain') == 'on' else False
-        df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'favourite'] = True if request.form.get('favourite') == 'on' else False
-        df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'garden'] = True if request.form.get('garden') == 'on' else False
-        df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'history'] = True if request.form.get('history') == 'on' else False
-        df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'late'] = True if request.form.get('late') == 'on' else False
-        df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'music'] = True if request.form.get('music') == 'on' else False
-        df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'pool'] = True if request.form.get('pool') == 'on' else False
-        df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'quiz'] = True if request.form.get('quiz') == 'on' else False
-        df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'roast'] = True if request.form.get('roast') == 'on' else False
-        df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'sport'] = True if request.form.get('sport') == 'on' else False
+        for review in list(Review2().__dict__.keys()):
+            if review not in self.ignore_list:
+                print(review)
+                df_reviews.loc[df_reviews['pub_identity'] == pub_id, review] = True if request.form.get(review) == 'on' else False
+            if review == 'detail':
+                df_reviews.loc[df_reviews['pub_identity'] == pub_id, review] = request.form[review]
+        # print('roast')
+        # print(df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'roast'])
+        # print('value')
+        # print(request.form.get('roast'))
+        #
+        # df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'brunch'] = True if request.form.get('brunch') == 'on' else False
+        # df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'dart'] = True if request.form.get('dart') == 'on' else False
+        # df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'entertain'] = True if request.form.get('entertain') == 'on' else False
+        # df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'favourite'] = True if request.form.get('favourite') == 'on' else False
+        # df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'garden'] = True if request.form.get('garden') == 'on' else False
+        # df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'history'] = True if request.form.get('history') == 'on' else False
+        # df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'late'] = True if request.form.get('late') == 'on' else False
+        # df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'music'] = True if request.form.get('music') == 'on' else False
+        # df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'pool'] = True if request.form.get('pool') == 'on' else False
+        # df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'quiz'] = True if request.form.get('quiz') == 'on' else False
+        # df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'roast'] = True if request.form.get('roast') == 'on' else False
+        # df_reviews.loc[df_reviews['pub_identity'] == pub_id, 'sport'] = True if request.form.get('sport') == 'on' else False
         # print(df_reviews.loc[df_reviews['pub_identity'] == pub_id])
         return df_reviews
