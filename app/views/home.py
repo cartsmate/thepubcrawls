c6582a7f87f162d6e8152ad1f08fbd03d68dfa19
Andy Carter <andyjcarter77@gmail.com>
2023-07-04 17:00:09 +0100

    draft typahead

--- app/views/home.py
+++ app/views/home.py
@@ -41,81 +41,20 @@ def home():
     # try:
     # df_pubs = S3().get_s3_pubs()
     df_pubs = Csv().get_pubs()
-    # df_pubs.to_csv(directory_path + '/files/pubs.csv', index=False, sep=',', encoding='utf-8')
-    # print(df_pubs)
+    list_of_pubs = df_pubs['pub_name'].values.tolist()
+    # list_of_pubs = df_pubs[['pub_identity', 'pub_name']].values.tolist()
+    # for pub in list_of_pubs:
+    #     json_pubs =
 
-    # df_reviews = S3().get_s3_reviews()
-    # df_reviews.to_csv(directory_path + '/files/reviews.csv', index=False, sep=',', encoding='utf-8')
-
-    # df_areas = S3().get_s3_areas()
-    # df_areas = Csv().get_areas()
-    # areas_json = Dataframes().df_to_dict(df_areas)
-    # df_areas.to_csv(directory_path + '/files/areas.csv', index=False, sep=',', encoding='utf-8')
-
-    # df_crawls = S3().get_s3_crawls()
-    # df_crawls.to_csv(directory_path + '/files/crawls.csv', index=False, sep=',', encoding='utf-8')
-
-    # df_stations = S3().get_s3_stations()
     df_stations = Csv().get_stations()
     stations_json = Dataframes().df_to_dict(df_stations)
 
     df_directions = Csv().get_directions()
-    # print('df_directions')
-    # print(df_directions)
-    # df_stations.to_csv(directory_path + '/files/stations.csv', index=False, sep=',', encoding='utf-8')
-
-    # df_photos = S3().get_s3_photos()
-    # df_photos.to_csv(directory_path + '/files/photos.csv', index=False, sep=',', encoding='utf-8')
-
-    # except botocore.exceptions.EndpointConnectionError:
-    #     df_pubs = Csv().get_pubs()
-    #     df_reviews = Csv().get_reviews()
-    #     df_areas = Csv().get_areas()
-    #     df_crawls = Csv().get_crawls()
-    #     df_photos = Csv().get_photos()
-
-    # start_time = time.time()
-    # df_pubs_trunc = df_pubs[['pub_identity', 'area_identity']]
-    # # print(df_pubs_trunc)
-    # df_pubs_trunc_group = df_pubs_trunc.groupby(['area_identity'], as_index=False).count()
-    # # print(df_pubs_trunc_group)
-    # df_areas_count = pd.merge(df_pubs_trunc_group, df_areas, how='left', on='area_identity')\
-    #     .sort_values(by=['area_name'], ascending=True)[['area_name']]
-    # # print(df_areas_count)
-    # list_areas = df_areas_count.values.tolist()
-    # print(list_areas)
-    #
-    # print("--- %s seconds ---" % (time.time() - start_time))
-
-    # start_time = time.time()
-    # df_pubs_unique = df_pubs['area_identity'].unique()
-    # df_x = pd.DataFrame({'area_identity': df_pubs_unique})
-    # list_areas = pd.merge(df_areas, df_x, how='inner', on=['area_identity'])[['area_name']].values.tolist()
-    # print(list_areas)
-    # print("--- %s seconds ---" % (time.time() - start_time))
-
-    # df_areas = Csv().get_areas()
-    # df_pub_with_area = pd.merge(df_pubs, df_areas, on='area_identity', how='left').sort_values(by='area_name')
-    # df_area_list = df_pub_with_area['area_name']
-    # df_area_unique = df_area_list.unique()
-    # list_areas = df_area_unique.tolist()
-
-    # areas_json = Dataframes().df_to_dict(df_areas_count)
-    # df_pubs_unique = df_pubs['station_identity'].unique()
-    # df_x = pd.DataFrame({'station_identity': df_pubs_unique})
-    # list_stations = pd.merge(df_stations, df_x, how='inner', on=['station_identity'])[['station_name']].values.tolist()
 
     df_pub_with_station = pd.merge(df_pubs, df_stations, on='station_identity', how='left').sort_values(by='station_name')
     df_station_list = df_pub_with_station['station_name']
     df_station_unique = df_station_list.unique()
     list_stations = df_station_unique.tolist()
-
-    # df_pub_station_with_direction = pd.merge(df_pub_with_station, df_directions, on='direction_identity', how='left')
-    # df_directions_unique = df_pub_station_with_direction['direction_identity'].unique()
-    # df_unique = pd.DataFrame({'direction_identity': df_directions_unique})
-
-
-    # list_directions = df_directions_unique.tolist()
 
     unique_station_identity_list = df_pub_with_station['station_identity'].unique()
     df_unique_stations_identity = pd.DataFrame({'station_identity': unique_station_identity_list})
@@ -129,62 +68,12 @@ def home():
     df_unique_directions = pd.merge(df_unique_directions_identity, df_directions, on='direction_identity', how='left')
     df_unique_directions_trunc = df_unique_directions[['direction_identity', 'direction_name']]
     directions_list = df_unique_directions_trunc.values.tolist()
-    # df_directions_trunc = df_directions[['direction_identity', 'direction_name']]
-    # directions_list = df_directions_trunc.values.tolist()
 
-    # PUB CRAWLS
-    # df_crawl_last = df_crawls.tail(1)
-    # start = df_crawl_last['start'].values[0]
-    # walk = df_crawl_last['walk'].apply(str).values[0]
-    # favourite = df_crawl_last['favourite'].values[0]
-    # stops = df_crawl_last['stops'].apply(str).values[0]
-    # criteria = df_crawl_last['criteria'].values[0]
-    # df_pub = df_pubs.loc[df_pubs['place'] == start]
-    # df_pub['colour'] = '#d9534f'
-    # pub_json = Dataframes().df_to_dict(df_pub)
-
-    # RANDOM PUB
-    # df_pubs['colour'] = '#0275d8'
-    # pubs_json = Dataframes().df_to_dict(df_pubs)
-    # pubs_total = df_pubs.shape[0]
-    # random_identity = random.randint(0, pubs_total)
-    # pub_random = df_pubs.iloc[[random_identity]]
-    # df = pd.merge(pub_random, df_photos, how='left', on='pub_identity')
-    # photo_id = df['photo_identity'].values[0]
-
-    # g = geocoder.ip('me')
-    # print(g.latlng)
-    # pub2 = Pub2()
-    # pub3 = pub2.__dict__
-    # for k, v in Review2().__dict__.items():
-    #     # print(k)
-    #     print(v.name)
-    #     print(v.alias)
-    #     print(v.icon)
-
-    # for p in pub2.__init__():
-    #     p
     l1 = list(Review2().__dict__.keys())
-    # for l in l1:
-    #     print(l)
+
     ignore_list = ['review_deletion', 'review_identity', 'pub_identity', 'detail']
 
     l3 = [x for x in l1 if x not in ignore_list]
-
-    # df_all = Csv().get_pubs()
-    # df_all_less_del = df_all[df_all["pub_deletion"] == "False"]
-    # no_all = df_all_less_del.shape[0]
-    # print(no_all)
-
-    # df_pubs_less_del = df_pubs[df_pubs["pub_deletion"] == "False"]
-    # no_all = df_pubs_less_del.shape[0]
-    # print(no_all)
-    #
-    # df_group = df_pubs_less_del.groupby(['rank'], as_index=False).count()
-    # no_0 = df_group[df_group['rank'] == 0]['pub_identity'].item()
-    # print(no_0)
-    #
-    # no_reviewed = no_all - no_0
 
     list_L = df_pubs[['pub_latitude', 'pub_longitude']].values.tolist()
     _lat = []
@@ -211,7 +100,7 @@ def home():
                            #  walk=walk, favourite=favourite, stops=stops, criteria=criteria, photo_id=photo_id,
                            #  pubs=pubs_json, pub=pub_json,
                             config2=config2, form_type='home',
-                           counter=counter,
+                           counter=counter, list_of_pubs=list_of_pubs,
                            # no_all=no_all, no_reviewed=no_reviewed,
                            # no_all_2=no_all_2, no_reviewed_2=no_reviewed_2,
                             ignore_list=ignore_list, review_obj=Review2(), features=l3, icon_list=icon_list)
